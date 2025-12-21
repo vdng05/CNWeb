cd341031ea8cf09ff165c67cd3671cd77623b54a
Vũ Đăng <101622939+vuhaidangz@users.noreply.github.com>
2025-12-21 23:03:07 +0700

    PHT 6

---
+++ PHT/Chapter_6/cse485_chapter6/app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    public function showHomePage()
+    {
+        return "Chào mừng bạn đến với PHT Chương 6 - Laravel!";
+    }
+}
